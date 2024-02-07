05dbbb09d8231aacfdcd96b0e52e2df3f073b513
israelmark <israelmark98@gmail.com>
2024-02-07 15:51:09 +0200

    feat: adding user handling with mongoose

--- routes/users.ts
+++ routes/users.ts
@@ -1,52 +1,65 @@
 import { Router , Request , Response } from "express";
 const router = Router();
+import User from '../models/user.js'
+import purify from "../utils/sanitize.js";
+import { createUserValidator, updateUserValidator } from "../validation/userValidator";
 
-type User = {
-  name: string;
-  id: number;
-}
-let users: User[] = [];
 
-router.get("/getAllUsers", (req: Request, res: Response) => {
-  res.status(200).send(users);
-});
-router.post("/addUser", (req, res) => {
-  const { name } = req?.body;
-  const id = users.length + 1;
-  users.push({ name, id });
-  res.status(201).send({ message: "user added successfully" });
-});
-router.put("/updateUser/", (req: Request, res: Response) => {
-  const { id, name } = req?.query as { id: string, name: string };
- 
-
-  if (!id || !name) {
-    return res.status(404).send({ message: "user not found" });
+router.post("/createUser", async(req, res) => {
+  try {
+    Object.keys(req.body).forEach(key => {
+    req.body[key] = purify.sanitize(req.body[key])
+    })
+    const { error } = createUserValidator.validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const newUser = await User.create(req.body)
+    res.status(200).send(newUser)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({errorMessage: "create fail"})
   }
-  const userID: number = parseInt(id)
-  const userToUpdate = users.find((user) => user.id === userID);
-  if (!userToUpdate) {
-    return res.status(404).send({ message: "user not found" });
+});
+router.get("/getAllUsers", async (req: Request, res: Response) => {
+  try {
+    const users = await User.find({})
+    res.status(200).send(users)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({message: "something error when trying to get users"})
   }
+});
+
+router.put("/updateUser:/id", async(req: Request, res: Response) => {
   
-  userToUpdate.name = name;
-  res.status(200).send({ message: "user updated successfully" });
+  try {
+    if (typeof req.params.id === 'string') {
+      req.params.id = purify.sanitize(req.params.id)
+    }
+    const { error } = updateUserValidator.validate(req.params.id)
+    if(error) return res.status(400).send(error.details[0].message) 
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true},
+    )
+    if (!updatedUser) return res.status(404).send("something went wrong with the updating");
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ errorMessage: "update fail" })
+  }
 });
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+      if (!user) return res.status(400).send("the user with the given id was not found");
 
-router.delete("/delUser", (req: Request, res: Response) => {
-  const { id } = req.query;
-
-  if (!id) {
-    return res.status(400).send({ message: "Missing 'id' parameter" });
+    res.status(200).send("deleting user successfully")
+  } catch (error) {
+      console.error(error)
+    res.status(500).send("something went wrong when deleting")
   }
-  const userId = parseInt(id as string);
-  const deletedUserIndex = users.findIndex((user) => user.id === userId);
-  if (deletedUserIndex === -1) {
-    return res.status(404).send({ message: "User not found" });
-  }
-  users.splice(deletedUserIndex, 1);
-
-  res.status(200).send({ message: "User deleted successfully" });
 });
 
 
